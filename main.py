2af41dbc3a87dd886f341e6a6e8d8c974f85516b
KungaCoding <95586873+KungaCoding@users.noreply.github.com>
2022-12-04 19:53:28 -0500

    I can log in to spotify as a user now and also view things like DMX song ids

--- main.py
+++ main.py
@@ -1,53 +1,139 @@
-from flask import Flask
+
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials, SpotifyOAuth
-import argparse
-# from routes import urls_blueprint
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
 
-def get_artist(name):
-    results = sp.search(q='artist:' + name, type='artist')
+
+@app.route('/')
+def index():
+
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 2. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 3. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a> | ' \
+           f'<a href="/current_user">me</a> | ' \
+           f'<a href="/tracks">Tracks</a>' \
+
+
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop("token_info", None)
+    return redirect('/')
+
+
+@app.route('/playlists')
+def playlists():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user_playlists()
+
+
+@app.route('/currently_playing')
+def currently_playing():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    track = spotify.current_user_playing_track()
+    if not track is None:
+        return track
+    return "No track currently playing."
+
+
+@app.route('/current_user')
+def current_user():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_artist(spotify, name):
+    results = spotify.search(q='artist:' + name, type='artist')
     items = results['artists']['items']
     if len(items) > 0:
         return items[0]
     else:
         return None
 
-def get_albums(artist):
+def get_albums(spotify, artist):
     albums = []
-    results = sp.artist_albums(artist['id'], album_type='album')
+    results = spotify.artist_albums(artist['id'], album_type='album')
     albums.extend(results['items'])
     return albums
 
 
-def get_tracks(album):
+def get_tracks(spotify, album):
     tracks = []
-    results = sp.album_tracks(album['id'])
+    results = spotify.album_tracks(album['id'])
     for result in results['items']:
         tracks.append(result['id'])
     return tracks
 
 
-def show_artist_tracks(artist):
-    albums = get_albums(artist)
+def show_artist_tracks(spotify, artist):
+    albums = get_albums(spotify, artist)
     page = ""
     for album in albums:
-        tracks = get_tracks(album)
+        tracks = get_tracks(spotify, album)
         page += str(tracks)
     return page
 
 
-@app.route('/', methods=['GET'])
-def index():
-    artist = get_artist('DMX')
+@app.route('/tracks', methods=['GET'])
+def tracks():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    artist = get_artist(spotify, 'DMX')
     # album = sp.artist_albums(artist['id'], album_type='album')
-    return show_artist_tracks(artist)
+    return show_artist_tracks(spotify, artist)
 
 if __name__ == '__main__':
-    client_credentials_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    # main()
-    app.run(port=5001)
-
-
+    app.run(threaded=True, port=int(os.environ.get("PORT",
+                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))

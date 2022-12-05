1ce9c5404a3739ee8e52d437d0ab4459ebbf7245
KungaCoding <95586873+KungaCoding@users.noreply.github.com>
2022-12-04 19:23:10 -0500

    it's cleaner

--- main.py
+++ main.py
@@ -6,24 +6,6 @@ import argparse
 
 app = Flask(__name__)
 
-# app.register_blueprint(urls_blueprint)
-
-# SPOTIPY_CLIENT_ID = '68e71e0a3b2344d4942c2449a217a59e'
-# SPOTIPY_CLIENT_SECRET = '8150ee0dfe834c079ad6b7ab9d8bbe61'
-# #
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
-#                                                client_secret=SPOTIPY_CLIENT_SECRET,
-#                                                redirect_uri='https://kasperowski.com/',
-#                                                scope="user-library-read"))
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Shows albums and tracks for given artist')
-    parser.add_argument('-a', '--artist', required=True,
-                        help='Name of Artist')
-    return parser.parse_args()
-
-
 def get_artist(name):
     results = sp.search(q='artist:' + name, type='artist')
     items = results['artists']['items']
@@ -31,38 +13,6 @@ def get_artist(name):
         return items[0]
     else:
         return None
-
-
-def show_album_tracks(album):
-    tracks = []
-    results = sp.album_tracks(album['id'])
-    tracks.extend(results['items'])
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
-    for i, track in enumerate(tracks):
-        print('%s. %s', i+1, track['name'])
-    return track['name']
-
-
-
-def show_artist(artist):
-    page = """"===={}====""".format(artist['name'])
-    page += """Popularity: {}""".format(artist['popularity'])
-    if len(artist['genres']) > 0:
-        page += """Genres: {}""".format(','.join(artist['genres']))
-    return page
-
-
-def main():
-    # args = get_args()
-    # artist = get_artist(args.artist)
-
-    # artist_input = input("Search for an Artist: ")
-    artist = get_artist('DMX')
-    show_artist(artist)
-
-
 
 def get_albums(artist):
     albums = []
@@ -104,16 +54,5 @@ if __name__ == '__main__':
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     # main()
     app.run(port=5001)
-
-
-
-# auth_manager = SpotifyClientCredentials()
-#
-# scope = "user-read-playback-state,user-modify-playback-state"
-# sp = spotipy.Spotify(auth_manager=auth_manager)
-#
-#
-# # Change track
-# uris=['spotify:track:6gdLoMygLsgktydTQ71b15']
 
 
